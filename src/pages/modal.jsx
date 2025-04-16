cf2ced8c2261e53a9d898ea048aad8487e047914
snehasrithumma <snehasrithumma44@gmail.com>
2025-04-16 13:26:50 -0700

    Edit project deatils dialog redesign updates

--- src/pages/modal.jsx
+++ src/pages/modal.jsx
@@ -1,11 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Button from '../components/Button'
-import styles from './ProjectDetail.module.css'
+import styles from './modal.module.css'
 
 export default function Modal({ project, isOpen, onSave, onClose }) {
   const [title, setTitle] = useState('')
+  const inputref = useRef(null)
+
+  useEffect(() => {
+    isOpen && inputref?.current?.focus()
+  }, [isOpen])
+
   if (!isOpen) {
-    return
+    return null
   }
 
   const handleSave = (event) => {
@@ -17,29 +23,31 @@ export default function Modal({ project, isOpen, onSave, onClose }) {
   return (
     <div className={styles.dialogOverlay} role="dialog" onClick={onClose}>
       <div
-        className={styles.dialogContents}
+        className={styles.dialog}
         onClick={(event) => event.stopPropagation()}
       >
-        <div className={styles.dialogTitle}>
-          <h4>Edit Project</h4>
-          <button onClick={onClose}>X</button>
+        <div className={styles.dialogHeader}>
+          <h2>Edit Project Details</h2>
+          <Button className={styles.closeButton} onClick={onClose}>
+            x
+          </Button>
         </div>
-        <div className={styles.mainContent}>
-          <form onSubmit={handleSave}>
-            <div>
-              <label htmlFor="projetcTitle">Title</label>
-              <input
-                required
-                type="text"
-                id="projetcTitle"
-                name="name"
-                onChange={(event) => setTitle(event.target.value)}
-              />{' '}
-            </div>
-            <Button className={styles.editButton} type="submit">
-              Submit
-            </Button>
-          </form>
+        <div className={styles.dialogBody}>
+          <div>
+            <label htmlFor="project-name">Name</label>{' '}
+            <input
+              ref={inputref}
+              type="text"
+              id="project-name"
+              name="projectName"
+              onChange={(event) => setTitle(event.target.value)}
+            />
+          </div>
+        </div>
+        <div className={styles.dialogActions}>
+          <Button className={styles.editButton} onClick={handleSave}>
+            Save
+          </Button>
         </div>
       </div>
     </div>
